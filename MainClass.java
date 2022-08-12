2ebf79f384b21d76e7a960930f17ed792989916e
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-12 22:10:43 +0530

    Added : simple welcome msg in main class

---
+++ MainClass.java
@@ -0,0 +1,8 @@
+package TDD;
+
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("Assignment No. 30\n");
+		System.out.println("Cab Invoice Generator");
+	}
+}
